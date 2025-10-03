883452115bfefe3a59192b6fa4c0c60ccc348c4c
lucas rondanorte <lucasrondanorte@gmail.com>
2025-10-03 18:38:58 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/agenda/',             // 👈 clave para GitHub Pages
+  base: '/agenda/',             // <- imprescindible para GitHub Pages
   define: { 'process.env': {} } // evita "process is not defined" en prod
 })
+
